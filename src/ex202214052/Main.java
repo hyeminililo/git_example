61ce26d78478ae5118fab2a599c4e4718fcd3460
hyeminililo <ng040115@gmail.com>
2024-09-25 02:24:53 +0900

    third

--- src/ex202214052/Main.java
+++ src/ex202214052/Main.java
@@ -2,9 +2,9 @@ package ex202214052;
 
 public class Main {
 
-	  public static void main(String[] args) {
-	    System.out.println("남궁혜민");
-	  }
+  public static void main(String[] args) {
+    System.out.println("남궁혜민");
+    System.out.println("하나");
+  }
 
-	}
-
+}
